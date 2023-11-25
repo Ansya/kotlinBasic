bdb65d06adf8abc61f90c0ec837a9664b7de2cb5
ansya <anastasia.kuimova@gmail.com>
2023-11-25 13:14:20 +0300

    KB-1-5 fix issue with constants

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,16 +1,16 @@
 package lesson_1
 
-const val ConversionRatio = 60
+const val CONVERSION_RATIO = 60
 
 fun main() {
     var seconds = 6480
     val hours: Int
     var minutes: Int
 
-    minutes = seconds / ConversionRatio
-    seconds %= ConversionRatio
-    hours = minutes / ConversionRatio
-    minutes %= ConversionRatio
+    minutes = seconds / CONVERSION_RATIO
+    seconds %= CONVERSION_RATIO
+    hours = minutes / CONVERSION_RATIO
+    minutes %= CONVERSION_RATIO
 
 
     println("Time spent by Yuri Gagarin in space is %02d:%02d:%02d".format(hours, minutes, seconds))
