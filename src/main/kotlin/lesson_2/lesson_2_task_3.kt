30f17870bb677616a808fbcb03d9b29d70f72823
ansya <anastasia.kuimova@gmail.com>
2023-11-25 13:27:12 +0300

    KB-2-3 Fix issue with constant name

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,6 +1,6 @@
 package lesson_2
 
-const val ConversionRatio = 60
+const val CONVERSION_RATIO = 60
 fun main() {
     val trainDepartureTimeHours = 9
     val trainDepartureTimeMinutes = 39
@@ -9,9 +9,9 @@ fun main() {
     println("Train departure time is %02d:%02d".format(trainDepartureTimeHours, trainDepartureTimeMinutes))
     println("Train will be in travel $trainTravelTimeInMinutes minutes")
 
-    val trainArrivalTimeInMinutes = (trainTravelTimeInMinutes + trainDepartureTimeMinutes) % ConversionRatio
+    val trainArrivalTimeInMinutes = (trainTravelTimeInMinutes + trainDepartureTimeMinutes) % CONVERSION_RATIO
     val trainArrivalTimeInHours = trainDepartureTimeHours +
-            (trainTravelTimeInMinutes + trainDepartureTimeMinutes) / ConversionRatio
+            (trainTravelTimeInMinutes + trainDepartureTimeMinutes) / CONVERSION_RATIO
 
     println("Train arrival time is %02d:%02d".format(trainArrivalTimeInHours, trainArrivalTimeInMinutes))
 }
