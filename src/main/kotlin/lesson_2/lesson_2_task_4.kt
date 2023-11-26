7c92de0e86c1a126e3302cb6b352e2cfa7f82290
ansya <anastasia.kuimova@gmail.com>
2023-11-26 14:16:03 +0300

    KB-2-4 fix issue with percent constant

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,12 +1,13 @@
 package lesson_2
 
-const val BAFF = 0.2
+const val BAFF = 20.0f
+const val ONE_HUNDRED_PERCENT = 100.0f
 fun main() {
     val cristalOre = 7
     val ironOre = 11
 
-    val cristalOreAsBaff = cristalOre * BAFF
-    val ironOreAsBaff = ironOre * BAFF
+    val cristalOreAsBaff = cristalOre * (BAFF / ONE_HUNDRED_PERCENT)
+    val ironOreAsBaff = ironOre * (BAFF / ONE_HUNDRED_PERCENT)
 
     println("$cristalOre(+ %.0f baff) cristal ore".format(cristalOreAsBaff))
     println("$ironOre(+ %.0f baff) iron ore".format(ironOreAsBaff))
