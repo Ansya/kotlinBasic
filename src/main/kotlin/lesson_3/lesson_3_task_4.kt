9330fd1caeec8a2df517570db2ae22f791dff91c
ansya <anastasia.kuimova@gmail.com>
2023-11-26 15:01:47 +0300

    KB-3-4 chess move added

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+    var moveFrom = "E2"
+    var moveWere = "E4"
+    var chessMoveNumber = 1
+
+    println("$moveFrom, $moveWere, $chessMoveNumber")
+
+    moveFrom = "D2"
+    moveWere = "D3"
+
+    println("$moveFrom, $moveWere, ${++chessMoveNumber}")
+}
