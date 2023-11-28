c757fdc98934cea13dba680ed4a6c1cd42cb1106
ansya <anastasia.kuimova@gmail.com>
2023-11-28 13:40:20 +0300

    KB-4-2 remove buildString

--- src/main/kotlin/lesson_4/lesson_4_task_2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -8,21 +8,17 @@ fun main() {
     var cargoVolume = 80
 
     println(
-        buildString {
-            append("A cargo with a weight of $cargoWeight kg and a volume of $cargoVolume liters ")
-            append("corresponds to the 'Average' category: ")
-            append((cargoWeight >= WEIGHT_MIN) && (cargoWeight <= WEIGHT_MAX) && (cargoVolume <= VOLUME_MIN))
-        }
+        "A cargo with a weight of $cargoWeight kg and a volume of $cargoVolume liters " +
+                "corresponds to the 'Average' category: " +
+                "${(cargoWeight >= WEIGHT_MIN) && (cargoWeight <= WEIGHT_MAX) && (cargoVolume <= VOLUME_MIN)}"
     )
 
     cargoWeight = 50
     cargoVolume = 100
     println(
-        buildString {
-            append("A cargo with a weight of $cargoWeight kg and a volume of $cargoVolume liters ")
-            append("corresponds to the 'Average' category: ")
-            append((cargoWeight >= WEIGHT_MIN) && (cargoWeight <= WEIGHT_MAX) && (cargoVolume <= VOLUME_MIN))
-        }
+        "A cargo with a weight of $cargoWeight kg and a volume of $cargoVolume liters " +
+                "corresponds to the 'Average' category: " +
+                "${(cargoWeight >= WEIGHT_MIN) && (cargoWeight <= WEIGHT_MAX) && (cargoVolume <= VOLUME_MIN)}"
     )
 
 }
