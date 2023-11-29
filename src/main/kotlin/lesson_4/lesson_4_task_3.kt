e471fbaaa0dab134206ecc611b6c487df1c39f8a
ansya <anastasia.kuimova@gmail.com>
2023-11-29 13:38:50 +0300

    KB-4-3 fix issue with season

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,19 +1,19 @@
 package lesson_4
 
-const val FAVORABLE_WEATHER = "sunny"
+const val FAVORABLE_WEATHER_IS_SUNNY = true
 const val FAVORABLE_AWNING_OPEN = true
 const val FAVORABLE_AIR_HUMIDITY = 20
-const val FAVORABLE_SEASON = "summer"
+const val FAVORABLE_SEASONS = "summer,autumn,spring"
 
 fun main() {
-    val weather = "sunny"
+    val isWeatherSunny = true
     val awningOpen = true
     val airHumidity = 20
     val season = "winter"
 
     println(
         "Are the conditions now favorable for the growth of legumes? - " +
-            "${(weather == FAVORABLE_WEATHER) && (awningOpen == FAVORABLE_AWNING_OPEN) &&
-                (airHumidity == FAVORABLE_AIR_HUMIDITY) && (season == FAVORABLE_SEASON)}"
+            "${(isWeatherSunny == FAVORABLE_WEATHER_IS_SUNNY) && (awningOpen == FAVORABLE_AWNING_OPEN) &&
+                (airHumidity == FAVORABLE_AIR_HUMIDITY) && FAVORABLE_SEASONS.contains(season)}"
     )
 }
