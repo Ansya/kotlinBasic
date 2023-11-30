a5fba9a93a4fc2f66268317f4cccc48837730df6
ansya <anastasia.kuimova@gmail.com>
2023-11-30 15:08:24 +0300

    KB-5-3 add lottery

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,23 @@
+package lesson_5
+
+const val FIRST_VALUE = 17
+const val SECOND_VALUE = 38
+
+fun main() {
+    println("To win in lottery enter two numbers from 0 till 42.")
+    println("First number:")
+    val firstValue = readln().toInt()
+    println("Second number:")
+    val secondValue = readln().toInt()
+
+    if ((firstValue == FIRST_VALUE && secondValue == SECOND_VALUE) ||
+        (firstValue == SECOND_VALUE && secondValue == FIRST_VALUE)) {
+        println("Congratulations! You won the main prize!")
+    } else if (firstValue == FIRST_VALUE || secondValue == FIRST_VALUE) {
+        println("You won a consolation prize!")
+    } else if (firstValue == SECOND_VALUE || secondValue == SECOND_VALUE) {
+        println("You won a consolation prize!")
+    } else {
+        println("Failure.")
+    }
+}
