9b1ec586cdb30b85801beb60ac5f26474a3e3e5c
ansya <anastasia.kuimova@gmail.com>
2023-12-01 13:36:12 +0300

    KB-4-3 Fix issue with season detection

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -3,17 +3,17 @@ package lesson_4
 const val FAVORABLE_WEATHER_IS_SUNNY = true
 const val FAVORABLE_AWNING_OPEN = true
 const val FAVORABLE_AIR_HUMIDITY = 20
-const val FAVORABLE_SEASONS = "summer,autumn,spring"
+const val FAVORABLE_SEASON_IS_SUMMER = true
 
 fun main() {
     val isWeatherSunny = true
     val awningOpen = true
     val airHumidity = 20
-    val season = "winter"
+    val seasonIsSummer = false
 
     println(
         "Are the conditions now favorable for the growth of legumes? - " +
             "${(isWeatherSunny == FAVORABLE_WEATHER_IS_SUNNY) && (awningOpen == FAVORABLE_AWNING_OPEN) &&
-                (airHumidity == FAVORABLE_AIR_HUMIDITY) && FAVORABLE_SEASONS.contains(season)}"
+                (airHumidity == FAVORABLE_AIR_HUMIDITY) && seasonIsSummer == FAVORABLE_SEASON_IS_SUMMER}"
     )
 }
