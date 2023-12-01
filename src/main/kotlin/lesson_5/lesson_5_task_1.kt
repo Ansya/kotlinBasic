8c24655eb83ff989b8d03c30fddb9a52225cfc2d
ansya <anastasia.kuimova@gmail.com>
2023-12-01 14:05:36 +0300

    KB-5-1 add constant with values range

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,8 +1,10 @@
 package lesson_5
 
+val VALUES_RANGE = 0..100
+
 fun main() {
-    val firstValue = (0..100).random()
-    val secondValue = (0..100).random()
+    val firstValue = VALUES_RANGE.random()
+    val secondValue = VALUES_RANGE.random()
 
     println("Prove that you are not a robot, please. Solve a simple example ($firstValue + $secondValue) = ")
     val result = readln().toInt()
