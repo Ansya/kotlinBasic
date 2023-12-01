474c942d8cc67812ef006832abfd09c444cb1b81
ansya <anastasia.kuimova@gmail.com>
2023-12-01 13:49:08 +0300

    KB-4-4 optimise the code: remove extra println and create isEven variable

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -1,20 +1,17 @@
 package lesson_4
 
 fun main() {
-    val exerciseForHands = true
-    val exerciseForLags = false
-    val exerciseForBack = false
-    val exerciseForPress = true
-
     val dayOfExercise = 5
-    val exersisesPlan = "Exercises for hans:  %b\n" +
-        "Exercises for lags: %b\n" +
-        "Exercises for back: %b\n" +
-        "Exercises for press: %b"
+    val isEven = (dayOfExercise % 2 == 0)
+    val exerciseForHands = !isEven
+    val exerciseForLags = isEven
+    val exerciseForBack = isEven
+    val exerciseForPress = !isEven
 
-    if (dayOfExercise % 2 == 0) {
-        println(exersisesPlan.format(!exerciseForHands, !exerciseForLags, !exerciseForBack, !exerciseForPress))
-    } else {
-        println(exersisesPlan.format(exerciseForHands, exerciseForLags, exerciseForBack, exerciseForPress))
-    }
+    val exersisesPlan = "Exercises for hans:  $exerciseForHands\n" +
+        "Exercises for lags: $exerciseForLags\n" +
+        "Exercises for back: $exerciseForBack\n" +
+        "Exercises for press: $exerciseForPress"
+
+    println(exersisesPlan)
 }
