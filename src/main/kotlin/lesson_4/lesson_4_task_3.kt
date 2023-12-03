e4ff794e7a2d1c0c1fa18489518658de5a025c82
ansya <anastasia.kuimova@gmail.com>
2023-12-03 14:23:27 +0300

    KB-4-3 Fix issue with variable type

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -3,17 +3,17 @@ package lesson_4
 const val FAVORABLE_WEATHER_IS_SUNNY = true
 const val FAVORABLE_AWNING_OPEN = true
 const val FAVORABLE_AIR_HUMIDITY = 20
-const val FAVORABLE_SEASON_IS_SUMMER = true
+const val UNFAVORABLE_SEASONS = "winter"
 
 fun main() {
     val isWeatherSunny = true
     val awningOpen = true
     val airHumidity = 20
-    val seasonIsSummer = false
+    val season = "winter"
 
     println(
         "Are the conditions now favorable for the growth of legumes? - " +
             "${(isWeatherSunny == FAVORABLE_WEATHER_IS_SUNNY) && (awningOpen == FAVORABLE_AWNING_OPEN) &&
-                (airHumidity == FAVORABLE_AIR_HUMIDITY) && seasonIsSummer == FAVORABLE_SEASON_IS_SUMMER}"
+                (airHumidity == FAVORABLE_AIR_HUMIDITY) && season != UNFAVORABLE_SEASONS}"
     )
 }
