cc1833b1b2392325d3d5558cf8741aaeb3798e5c
ansya <anastasia.kuimova@gmail.com>
2023-12-04 14:22:40 +0300

    KB-5-1 fix issue with range value

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,10 +1,9 @@
 package lesson_5
 
-val VALUES_RANGE = 0..100
-
 fun main() {
-    val firstValue = VALUES_RANGE.random()
-    val secondValue = VALUES_RANGE.random()
+    val valuesRange = 0..100
+    val firstValue = valuesRange.random()
+    val secondValue = valuesRange.random()
 
     println("Prove that you are not a robot, please. Solve a simple example ($firstValue + $secondValue) = ")
     val result = readln().toInt()
