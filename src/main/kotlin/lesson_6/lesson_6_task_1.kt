d7ef1bba3fb7cb7c4b027541b43d4cb4811c790b
ansya <anastasia.kuimova@gmail.com>
2023-12-05 15:08:54 +0300

    KB-6-1 add missed code

--- src/main/kotlin/lesson_6/lesson_6_task_1.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -1,1 +1,24 @@
-ˆ
+package lesson_6
+
+fun main() {
+    println("To register please create your login:")
+    val userLogin = readLine()!!
+    println("Please, create your password:")
+    val userPass = readLine()!!
+    println("Great, you are register now.")
+    var login: String
+    var pass: String
+
+    do {
+        println("To authorise, please enter your login:")
+        login = readLine()!!
+        println("enter your password:")
+        pass = readLine()!!
+
+        val authMessage = when {
+            (login == userLogin) && (pass == userPass) -> "Authorization successful!!!"
+            else -> "Authorization fail."
+        }
+        println(authMessage)
+    } while ((login != userLogin) || (pass != userPass))
+}
