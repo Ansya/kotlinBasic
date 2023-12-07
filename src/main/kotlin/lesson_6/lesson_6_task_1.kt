6d1fbcc0e1603b5292321af1becdf87743d467e0
ansya <anastasia.kuimova@gmail.com>
2023-12-07 07:37:35 +0300

    KB-6-1 use readln instead of readLine

--- src/main/kotlin/lesson_6/lesson_6_task_1.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -2,18 +2,18 @@ package lesson_6
 
 fun main() {
     println("To register please create your login:")
-    val userLogin = readLine()!!
+    val userLogin = readln()
     println("Please, create your password:")
-    val userPass = readLine()!!
+    val userPass = readln()
     println("Great, you are register now.")
     var login: String
     var pass: String
 
     do {
         println("To authorise, please enter your login:")
-        login = readLine()!!
+        login = readln()
         println("enter your password:")
-        pass = readLine()!!
+        pass = readln()
 
         val authMessage = when {
             (login == userLogin) && (pass == userPass) -> "Authorization successful!!!"
