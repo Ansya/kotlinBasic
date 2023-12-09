ac9ee5a3eef93611917b3998afc6f8ba3cf730be
ansya <anastasia.kuimova@gmail.com>
2023-12-09 07:26:38 +0300

    KB-6-5 remove code of authorisation

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -1,40 +1,24 @@
 package lesson_6
 
-const val USER_NAME = "Zaphod"
-const val USER_PASSWORD = "PanGalactic"
 fun main() {
-    println("To authorise please enter your user name:")
-    val userName = readln()
-    println("Please enter you password:")
-    val pass = readln()
+    val valuesRange = 1..9
+    var firstValue: Int
+    var secondValue: Int
+    var result: Int
+    var numberOfAttempts = 3
 
-    if (userName == USER_NAME) {
-        if (pass == USER_PASSWORD) {
-            val valuesRange = 1..9
-            var firstValue: Int
-            var secondValue: Int
-            var result: Int
-            var numberOfAttempts = 3
+    do {
+        --numberOfAttempts
+        firstValue = valuesRange.random()
+        secondValue = valuesRange.random()
+        println("Prove that you are not a robot, please. Solve a simple example ($firstValue + $secondValue):")
+        result = readln().toInt()
 
-            do {
-                --numberOfAttempts
-                firstValue = valuesRange.random()
-                secondValue = valuesRange.random()
-                println("Prove that you are not a robot, please. Solve a simple example " +
-                            "($firstValue + $secondValue) = ")
-                result = readln().toInt()
-
-                if (result == firstValue + secondValue) {
-                    println("Welcome!")
-                    break
-                } else {
-                    println("Access denied.")
-                }
-            } while (numberOfAttempts > 0)
+        if (result == firstValue + secondValue) {
+            println("Welcome!")
+            break
         } else {
-            println("Authorisation failed.")
+            println("Access denied.")
         }
-    } else {
-        println("Looks like you are not registered yet on the \"Heart of Gold\". \nPlease register first.")
-    }
+    } while (numberOfAttempts > 0)
 }
