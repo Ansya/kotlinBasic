8c3a1c947f39377613e28a5088d5b1de983827fd
ansya <anastasia.kuimova@gmail.com>
2023-12-10 13:54:07 +0300

    KB-8-1 reduce code by using sun() fun

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,11 +1,11 @@
 package lesson_8
 
+import java.util.Arrays
+
 fun main() {
     val numberOfAdViews = intArrayOf(43, 56, 34, 45, 54, 78, 90)
-    var numberOfAdViewsPerWeek = 0
+    val numberOfAdViewsPerWeek = numberOfAdViews.sum()
 
-    for (adViews in numberOfAdViews) {
-        numberOfAdViewsPerWeek += adViews
-    }
+    println("Number of ad for every day: ${numberOfAdViews.contentToString()}")
     println("Number of ad views per week $numberOfAdViewsPerWeek")
 }
