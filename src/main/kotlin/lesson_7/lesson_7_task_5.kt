c41edc7128209ebd34b1c57f84c17b59e65738c8
ansya <anastasia.kuimova@gmail.com>
2023-12-10 13:49:29 +0300

    KB-7-5 remove redundant code and comment

--- src/main/kotlin/lesson_7/lesson_7_task_5.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_5.kt
@@ -2,25 +2,15 @@ package lesson_7
 
 const val PASSWORD_SIZE_MIN = 6
 fun main() {
-    val lowerCaseCharRangeForPassword: CharRange = 'a'..'z'
-    val upperCaseCharRangeForPassword: CharRange = 'A'..'Z'
-    val intRangeForPassword: IntRange = 0..9
-    val passwordElementsRange = 1..3
+    val rangeForPassword = ('a'..'z') + ('A'..'Z') + (0..9)
     var password = ""
 
     println("Please add password size (min value is 6):")
     var passwordSize = readln().toInt()
     if (passwordSize < PASSWORD_SIZE_MIN) passwordSize = PASSWORD_SIZE_MIN
 
-//    for (i in passwordSize downTo 1) {
     (0 until passwordSize).forEach {
-        val element = passwordElementsRange.random()
-
-        when (element) {
-            1 -> password += intRangeForPassword.random().toString()
-            2 -> password += upperCaseCharRangeForPassword.random()
-            3 -> password += lowerCaseCharRangeForPassword.random()
-        }
+        password += rangeForPassword.random().toString()
     }
     println("Generated password: $password")
 }
