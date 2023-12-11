f14b183d2748d613be44662798116b1ff3451689
ansya <anastasia.kuimova@gmail.com>
2023-12-11 14:53:20 +0300

    KB-9-3 calculation of ingredients amount for N number of portions is added

---
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -0,0 +1,15 @@
+package lesson_9
+
+fun main() {
+    val listOfIngredients = listOf("Чечевица", "Лук", "Помидор", "Морковь", "Чеснок")
+    // https://eda.ru/recepty/supy/sup-pjure-iz-chechevici-30588
+    val listOfIngredientAmount = listOf(3.5, 0.5, 1.0, 0.5, 0.5)
+
+    println("Сколько порций супа-пюре из чечевицы вы собираетесь приготовить?")
+    val numberOfPortions = readln().toInt()
+    println("На $numberOfPortions порций вам понадобится:")
+    for (ingredient in listOfIngredients) {
+        val index = listOfIngredients.indexOf(ingredient)
+        println("${index + 1}) $ingredient - ${listOfIngredientAmount[index] * numberOfPortions}")
+    }
+}
