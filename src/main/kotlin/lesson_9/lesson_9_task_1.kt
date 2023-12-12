a173907b64c48fc1edea5a512bf437f8656c0e3a
ansya <anastasia.kuimova@gmail.com>
2023-12-12 14:13:25 +0300

    KB-9-1 remove not optimised code with for in for loop

--- src/main/kotlin/lesson_9/lesson_9_task_1.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -3,7 +3,8 @@ package lesson_9
 fun main() {
     val listOfIngredients = listOf("Молоко", "Яйца", "Масло растительное", "Мука", "Сахар", "Соль")
     println("Список ингредиентов для Блинчиков:")
-    for (ingredient in listOfIngredients) {
-        println("${listOfIngredients.indexOf(ingredient) + 1}) $ingredient")
+
+    for (i in 0 until listOfIngredients.size) {
+        println("${i + 1}) ${listOfIngredients[i]}")
     }
 }
