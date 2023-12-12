cdc30ab52285fa8a8f70d545e53803e0515dd129
ansya <anastasia.kuimova@gmail.com>
2023-12-12 14:17:19 +0300

    KB-9-3 remove not optimised code with for in for loop

--- src/main/kotlin/lesson_9/lesson_9_task_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -8,8 +8,8 @@ fun main() {
     println("Сколько порций супа-пюре из чечевицы вы собираетесь приготовить?")
     val numberOfPortions = readln().toInt()
     println("На $numberOfPortions порций вам понадобится:")
-    for (ingredient in listOfIngredients) {
-        val index = listOfIngredients.indexOf(ingredient)
-        println("${index + 1}) $ingredient - ${listOfIngredientAmount[index] * numberOfPortions}")
+
+    for (i in 0 until listOfIngredients.size) {
+        println("${i + 1}) ${listOfIngredients[i]} - ${listOfIngredientAmount[i] * numberOfPortions}")
     }
 }
