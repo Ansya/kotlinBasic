70f7f21d904a140adc2ad48dcfd09175d212df90
ansya <anastasia.kuimova@gmail.com>
2023-12-12 14:04:18 +0300

    KB-9-4 sorted list of ingredients entering is added

---
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -0,0 +1,7 @@
+package lesson_9
+
+fun main() {
+    println("Введите пять названий ингредиентов блюда в одной строке, разделив их запятой с пробелом:")
+    val listOfIngredients = readln().split(", ").toList()
+    println("Вы ввели следующий список: ${listOfIngredients.sorted()}")
+}
