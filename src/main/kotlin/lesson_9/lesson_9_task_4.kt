d92e8924ca4ee9e2a5d043cc9329cf0a45998481
ansya <anastasia.kuimova@gmail.com>
2023-12-13 13:45:29 +0300

    KB-9-5 recipe ingredients entering is added with sort and duplicates removing

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -2,6 +2,6 @@ package lesson_9
 
 fun main() {
     println("Введите пять названий ингредиентов блюда в одной строке, разделив их запятой с пробелом:")
-    val listOfIngredients = readln().split(", ").toList()
+    val listOfIngredients = readln().split(", ")
     println("Вы ввели следующий список: ${listOfIngredients.sorted()}")
 }
