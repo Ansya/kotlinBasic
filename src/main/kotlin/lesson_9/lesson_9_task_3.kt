0254f6c15d747dca22731d5398aff0032833b75b
ansya <anastasia.kuimova@gmail.com>
2023-12-14 13:38:38 +0300

    KB-9-3 Change login according to lesson task

--- src/main/kotlin/lesson_9/lesson_9_task_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -1,15 +1,12 @@
 package lesson_9
 
 fun main() {
-    val listOfIngredients = listOf("Чечевица", "Лук", "Помидор", "Морковь", "Чеснок")
-    // https://eda.ru/recepty/supy/sup-pjure-iz-chechevici-30588
-    val listOfIngredientAmount = listOf(3.5, 0.5, 1.0, 0.5, 0.5)
+    val listOfIngredientAmount = listOf(2, 50, 15)
 
-    println("Сколько порций супа-пюре из чечевицы вы собираетесь приготовить?")
+    println("Сколько порций омлета вы собираетесь приготовить?")
     val numberOfPortions = readln().toInt()
-    println("На $numberOfPortions порций вам понадобится:")
-
-    for (i in 0 until listOfIngredients.size) {
-        println("${i + 1}) ${listOfIngredients[i]} - ${listOfIngredientAmount[i] * numberOfPortions}")
-    }
+    println("На $numberOfPortions порций вам понадобится: " +
+            "Яиц - ${listOfIngredientAmount[0] * numberOfPortions}, " +
+            "молока - ${listOfIngredientAmount[1] * numberOfPortions}, " +
+            "сливочного масла - ${listOfIngredientAmount[2] * numberOfPortions}.")
 }
