1e7116e6a621df51dae728c311cfaa0c8bbfad7c
ansya <anastasia.kuimova@gmail.com>
2023-12-14 14:09:40 +0300

    KB-9-5 change collection to Set

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -4,16 +4,16 @@ import java.util.*
 
 fun main() {
     println("Введите пять названий ингредиентов блюда, каждое в новой строке:")
-    val listOfIngredients = List(5) { readln() }.distinct().sorted()
+    val listOfIngredients = (List(5) { readln() }).toSortedSet()
 
     println("Вы ввели следующий список:")
-    for (i in 0 until listOfIngredients.size) {
-        if (i == 0) {
-            print(listOfIngredients[i].replaceFirstChar {
+    listOfIngredients.forEachIndexed { index, ingredient ->
+        if (index == 0) {
+            print(ingredient.replaceFirstChar {
                 if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
             })
         } else {
-            print(", ${listOfIngredients[i]}")
+            print(", $ingredient")
         }
     }
 }
