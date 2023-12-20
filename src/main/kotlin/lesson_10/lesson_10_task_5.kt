626af548f16c3953b392cf16f4d7d00187400417
ansya <anastasia.kuimova@gmail.com>
2023-12-20 13:43:04 +0300

    KB-10-5 implement the method to get the list of purchases

---
+++ src/main/kotlin/lesson_10/lesson_10_task_5.kt
@@ -0,0 +1,44 @@
+package lesson_10
+
+const val USER_NAME = "Zaphod"
+const val USER_PASSWORD = "PanGalactic"
+const val TOKEN_SIZE = 32
+
+fun main() {
+    println("To get list of purchases, please enter you user name:")
+    val userLogin = readln()
+    println("and you password:")
+    val userPassword = readln()
+
+    val userShoppingList = getShoppingList(authoriseTheUser(userLogin, userPassword))
+    if (userShoppingList == null) {
+        println("You are not authorised. Cannot list your shopping list.")
+    } else {
+        println("Your shopping list is: $userShoppingList")
+    }
+}
+
+fun authoriseTheUser(login: String, password: String): String? {
+    var token: String? = null
+    if (login == USER_NAME && password == USER_PASSWORD) {
+        token = tokenGenerator()
+    }
+    return token
+}
+
+fun tokenGenerator(): String {
+    var token = ""
+    val rangeOfToken = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    for (i in 0 until TOKEN_SIZE) {
+        token += rangeOfToken.random()
+    }
+    return token
+}
+
+fun getShoppingList(token: String?): List<String>? {
+    return if (token.isNullOrBlank()) {
+        null
+    } else {
+        listOf("paper", "paints", "brushes", "plasticine")
+    }
+}
