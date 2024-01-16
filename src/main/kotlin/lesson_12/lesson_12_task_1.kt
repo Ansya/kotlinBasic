044fd3274ae0eecd8df35fd8a7a69b100b2591d4
ansya <anastasia.kuimova@gmail.com>
2024-01-16 13:50:32 +0300

    KB-12-1 Weather class added

---
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -0,0 +1,29 @@
+package lesson_12
+
+class Weather(
+    var daytimeTemperature: Int = 0,
+    var eveningTemperature: Int = 0,
+    var precipitation: Boolean = false,
+) {
+    fun printWeather() {
+        println("Дневная температура: $daytimeTemperature")
+        println("Вечерняя температура: $eveningTemperature")
+        println("Наличие осадков: $precipitation")
+    }
+}
+
+fun main() {
+    val firstDayWeather = Weather()
+    firstDayWeather.daytimeTemperature = -5
+    firstDayWeather.eveningTemperature = -13
+    firstDayWeather.precipitation = true
+    println("День 1, погода:")
+    firstDayWeather.printWeather()
+
+    val secondDayWeather = Weather()
+    secondDayWeather.daytimeTemperature = -1
+    secondDayWeather.eveningTemperature = -4
+    secondDayWeather.precipitation = false
+    println("День 2, погода:")
+    secondDayWeather.printWeather()
+}
