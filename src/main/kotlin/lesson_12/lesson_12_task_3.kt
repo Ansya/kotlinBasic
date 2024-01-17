4b2b3a0036aa56209a24602893adc5848f3ef857
ansya <anastasia.kuimova@gmail.com>
2024-01-17 15:21:38 +0300

    KB-12-3 fix issue with constant type and name

--- src/main/kotlin/lesson_12/lesson_12_task_3.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_3.kt
@@ -1,14 +1,14 @@
 package lesson_12
 
-val KELVIN_COEFFICIENT = 273.15
+const val KELVIN_COEFFICIENT1 = 273.15
 
 class Weather2(
     _daytimeTemperature: Int,
     _eveningTemperature: Int,
     _isPrecipitation: Boolean,
 ) {
-    val daytimeTemperature = (_daytimeTemperature - KELVIN_COEFFICIENT).toInt()
-    val eveningTemperature = (_eveningTemperature - KELVIN_COEFFICIENT).toInt()
+    val daytimeTemperature = (_daytimeTemperature - KELVIN_COEFFICIENT1).toInt()
+    val eveningTemperature = (_eveningTemperature - KELVIN_COEFFICIENT1).toInt()
     val isPrecipitation = _isPrecipitation
     fun printWeather() {
         println("Дневная температура: $daytimeTemperature")
