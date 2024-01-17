81b09772796986eb582d44346d948667428c11fd
ansya <anastasia.kuimova@gmail.com>
2024-01-17 15:13:19 +0300

    KB-12-1 fix issue with parameter naming

--- src/main/kotlin/lesson_12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -3,12 +3,12 @@ package lesson_12
 class Weather() {
     var daytimeTemperature: Int = 0
     var eveningTemperature: Int = 0
-    var precipitation: Boolean = false
+    var isPrecipitation: Boolean = false
 
     fun printWeather() {
         println("Дневная температура: $daytimeTemperature")
         println("Вечерняя температура: $eveningTemperature")
-        println("Наличие осадков: $precipitation")
+        println("Наличие осадков: $isPrecipitation")
     }
 }
 
@@ -16,14 +16,14 @@ fun main() {
     val firstDayWeather = Weather()
     firstDayWeather.daytimeTemperature = -5
     firstDayWeather.eveningTemperature = -13
-    firstDayWeather.precipitation = true
+    firstDayWeather.isPrecipitation = true
     println("День 1, погода:")
     firstDayWeather.printWeather()
 
     val secondDayWeather = Weather()
     secondDayWeather.daytimeTemperature = -1
     secondDayWeather.eveningTemperature = -4
-    secondDayWeather.precipitation = false
+    secondDayWeather.isPrecipitation = false
     println("День 2, погода:")
     secondDayWeather.printWeather()
 }
