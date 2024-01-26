ed3c8368dd89396e7193e1d713cd10aa97645a65
ansya <anastasia.kuimova@gmail.com>
2024-01-26 16:11:53 +0300

    KB-14-1 reformat spaces

--- src/main/kotlin/lesson_14/lesson_14_task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -1,26 +1,26 @@
 package lesson_14
 
 open class Cargo(
-    val name :String,
-    val speed :Int = 15,
-    val crewMembersCapacity :Int = 20,
-    val carryingCapacity :Int = 2500,
+    val name: String,
+    val speed: Int = 15,
+    val crewMembersCapacity: Int = 20,
+    val carryingCapacity: Int = 2500,
 )
 
 class Liner(
-    name :String,
-    speed :Int = 25,
-    crewMembersCapacity :Int = 50,
-    carryingCapacity :Int = 500,
-    val passengerCapacity :Int = 200,
+    name: String,
+    speed: Int = 25,
+    crewMembersCapacity: Int = 50,
+    carryingCapacity: Int = 500,
+    val passengerCapacity: Int = 200,
 ) : Cargo(name, speed, crewMembersCapacity, carryingCapacity)
 
 class Icebreaker(
-    name :String,
-    speed :Int = 10,
-    crewMembersCapacity :Int = 10,
-    carryingCapacity :Int = 30,
-    val isBreakIce :Boolean = true,
+    name: String,
+    speed: Int = 10,
+    crewMembersCapacity: Int = 10,
+    carryingCapacity: Int = 30,
+    val isBreakIce: Boolean = true,
 ) : Cargo(name, speed, crewMembersCapacity, carryingCapacity)
 
 fun main() {
