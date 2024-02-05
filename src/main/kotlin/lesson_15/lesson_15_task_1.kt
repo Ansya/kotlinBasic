ac30deaa0968941f642083a4c59723db3494b085
ansya <anastasia.kuimova@gmail.com>
2024-02-05 12:17:53 +0300

    KB-15-1 add flying ang swimming creatures

---
+++ src/main/kotlin/lesson_15/lesson_15_task_1.kt
@@ -0,0 +1,69 @@
+package lesson_15
+
+interface Flying {
+    fun fly()
+}
+interface Swimming {
+    fun swim()
+}
+abstract class Fish : Swimming {
+    abstract val flippers: String
+}
+abstract class Waterbird : Flying, Swimming {
+    abstract val webbedFeet: String
+    abstract val wing: String
+}
+
+class Crucian(
+    val name :String,
+    override val flippers: String
+) : Fish() {
+    override fun swim() {
+        println("$name плывет под водой с помощью: $flippers")
+    }
+}
+
+class Duck(
+    val name :String,
+    override val webbedFeet: String,
+    override val wing: String,
+) : Waterbird() {
+    override fun fly() {
+        println("$name летит с помощью: $wing")
+    }
+
+    override fun swim() {
+        println("$name плывет над водой с помощью: $webbedFeet")
+    }
+}
+class Gull(
+    val name :String,
+    override val webbedFeet: String,
+    override val wing: String,
+) : Waterbird() {
+    override fun fly() {
+        println("$name летит с помощью: $wing")
+    }
+
+    override fun swim() {
+        println("$name плывет над водой с помощью: $webbedFeet")
+    }
+}
+
+fun main() {
+    val crucian = Crucian(name = "карась морской",
+        flippers = "плавники")
+    crucian.swim()
+
+    val dack = Duck(name = "кряква",
+        webbedFeet = "лапы с перепонками",
+        wing = "пестрые крылья")
+    dack.fly()
+    dack.swim()
+
+    val gull = Gull(name = "сизая чайка",
+        webbedFeet = "лапы с перепонками",
+        wing = "серые крылья")
+    gull.fly()
+    gull.swim()
+}
