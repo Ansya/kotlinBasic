3180aa907f7d6ad6b510317995e0f30671cfc1d3
ansya <anastasia.kuimova@gmail.com>
2024-02-05 17:57:25 +0300

    KB-15-3 add forum admin and forum user

---
+++ src/main/kotlin/lesson_15/lesson_15_task_3.kt
@@ -0,0 +1,52 @@
+package lesson_15
+
+import java.util.UUID
+
+abstract class Member {
+    abstract val id :UUID
+    abstract val name :String
+    abstract fun readMessage()
+    abstract fun writeMessage()
+}
+
+class ForumUser(
+    override val id: UUID = UUID.randomUUID(),
+    override val name: String,
+) :Member() {
+    override fun readMessage() {
+        println("Пользователь $name прочел сообщения форума.")
+    }
+    override fun writeMessage() {
+        println("Пользователь $name написал сообщение в форум.")
+    }
+}
+
+class ForumAdmin(
+    override val id: UUID = UUID.randomUUID(),
+    override val name: String,
+) :Member() {
+    override fun readMessage() {
+        println("Администратор $name прочел сообщения форума.")
+    }
+    override fun writeMessage() {
+        println("Администратор $name написал сообщение в форум.")
+    }
+    fun removeUser() {
+        println("Администратор $name удалил пользователя.")
+    }
+    fun removeMessage() {
+        println("Администратор $name удалил сообщение.")
+    }
+}
+
+fun main() {
+    val user1 = ForumUser(name = "Nick")
+    user1.readMessage()
+    user1.writeMessage()
+
+    val admin1 = ForumAdmin(name = "Kolya")
+    admin1.removeUser()
+    admin1.removeMessage()
+    admin1.readMessage()
+    admin1.writeMessage()
+}
