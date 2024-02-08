4901eea8af6798a939c047a80ba99f032e6af3f8
ansya <anastasia.kuimova@gmail.com>
2024-02-08 17:11:51 +0300

    KB-15-2 fix formatting and replace if to when

--- src/main/kotlin/lesson_15/lesson_15_task_2.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_2.kt
@@ -1,15 +1,17 @@
 package lesson_15
 
-abstract class WeatherStationStats(val value :Int)
-class Temperature(value: Int) :WeatherStationStats(value)
-class PrecipitationAmount(value: Int) :WeatherStationStats(value)
+abstract class WeatherStationStats(val value: Int)
+class Temperature(value: Int) : WeatherStationStats(value)
+class PrecipitationAmount(value: Int) : WeatherStationStats(value)
 
 class WeatherServer {
-    fun sendStatsToServer(statsValue :WeatherStationStats) {
-        if (statsValue is Temperature) {
-            println("Отправляю на сервер данные: Температура = ${statsValue.value}")
-        } else if (statsValue is PrecipitationAmount) {
-            println("Отправляю на сервер данные: Количество осадков = ${statsValue.value}")
+    fun sendStatsToServer(statsValue: WeatherStationStats) {
+        when (statsValue) {
+            is Temperature ->
+                println("Отправляю на сервер данные: Температура = ${statsValue.value}")
+
+            is PrecipitationAmount ->
+                println("Отправляю на сервер данные: Количество осадков = ${statsValue.value}")
         }
     }
 }
