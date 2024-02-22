15bf47e548b4fe0289b0905abd1294cdc5c1d683
ansya <anastasia.kuimova@gmail.com>
2024-02-22 10:21:33 +0300

    KB-19-4 remove functions with type name

--- src/main/kotlin/lesson_19/lesson_19_task_5.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_5.kt
@@ -1,43 +1,44 @@
 package lesson_19
 
-enum class Gender(val shot :String) {
-    MAN("м") {
-        override fun getGenderDescription(): String = "мужской"
-    },
-    WOMAN("ж") {
-        override fun getGenderDescription(): String = "женский"
-    };
-
-    abstract fun getGenderDescription(): String
+enum class Gender(
+    val shot: String,
+    val description: String,
+) {
+    MAN(shot = "м", description = "мужской"),
+    WOMAN(shot = "ж", description = "женский"),
+    UNDEFINDED(shot = "-", description = "неопределён")
 }
 
 class Human(
     val name: String,
     val gender: Gender,
 ) {
-    fun getHumanInfo(): String = "$name - ${gender.getGenderDescription()}"
+    fun getHumanInfo(): String = "$name - ${gender.description}"
 }
 
 fun main() {
     println("Давайте заполним картотеку.")
-    val humans = mutableListOf <Human>()
-    for(i in 1..5) {
+    val humans = mutableListOf<Human>()
+    for (i in 1..5) {
         println("Введите имя и пол одной буквой м или ж через пробел:")
         val value = readln().split(" ")
         when (value[1]) {
             Gender.WOMAN.shot -> {
-                val human = Human(name = value[0], Gender.WOMAN)
-                humans.add(human)
+                humans.add(Human(name = value[0], Gender.WOMAN))
             }
+
             Gender.MAN.shot -> {
-                val human = Human(name = value[0], Gender.MAN)
-                humans.add(human)
+                humans.add(Human(name = value[0], Gender.MAN))
+            }
+
+            else -> {
+                humans.add(Human(name = value[0], Gender.UNDEFINDED))
             }
         }
     }
     println()
     println("Каталог содержит следующие значения:")
-    humans.forEach{
+    humans.forEach {
         println(it.getHumanInfo())
     }
 }
