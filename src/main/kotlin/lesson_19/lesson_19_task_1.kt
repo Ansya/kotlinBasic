628ff4e2d6ef68fb7b4de3d9ed311805121b177e
ansya <anastasia.kuimova@gmail.com>
2024-02-22 08:28:07 +0300

    KB-19-1 replace functions with names to variables

--- src/main/kotlin/lesson_19/lesson_19_task_1.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_1.kt
@@ -1,31 +1,13 @@
 package lesson_19
 
-enum class Fish {
-    GUPPY {
-        override fun getFishTypeNaming(): String {
-            return "guppy"
-        }
-    },
-    ANGELFISH {
-        override fun getFishTypeNaming(): String {
-            return "angelfish"
-        }
-    },
-    GOLDFISH {
-        override fun getFishTypeNaming(): String {
-            return "goldfish"
-        }
-    },
-    SIAMESE_FIGHTING_FISH {
-        override fun getFishTypeNaming(): String {
-            return "siamese fighting fish"
-        }
-    };
-
-    abstract fun getFishTypeNaming() : String
+enum class Fish(val nameFish :String) {
+    GUPPY("guppy"),
+    ANGELFISH("angelfish"),
+    GOLDFISH("goldfish"),
+    SIAMESE_FIGHTING_FISH("siamese fighting fish"),
 }
 
 fun main() {
     println("You can add to you aquarium next fishes:")
-    for (i in Fish.entries) println(i.getFishTypeNaming())
+    for (i in Fish.entries) println(i.nameFish)
 }
