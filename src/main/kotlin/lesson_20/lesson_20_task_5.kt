9774f7650018b3489fe16c62d02b70ecde2daadf
ansya <anastasia.kuimova@gmail.com>
2024-02-27 11:24:22 +0300

    KB-20-5 Put phases into class Robot

--- src/main/kotlin/lesson_20/lesson_20_task_5.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_5.kt
@@ -2,29 +2,28 @@ package lesson_20
 
 import kotlin.random.Random
 
-class Robot(
-    val phrase: String,
-) {
+class Robot {
+    private val phrases = listOf(
+        "хороший денёк для хорошей беседы",
+        "цените мир небездушных людей пока нас не заменили роботы",
+        "роботов ещё не создали а люди уже не нужны",
+        "за созданием любого робота стоит человек поэтому и с роботом можно поговорить",
+        "чем больше я узнаю людей тем больше люблю робототехнику и компьютеры"
+    )
     var modifier: (String) -> String = { it }
     fun setRobotModifier(newModifier: (String) -> String) {
         modifier = newModifier
     }
 
     fun say() {
-        val phraseToSay = modifier(phrase)
+        val randomIndex = Random.nextInt(phrases.size)
+        val phraseToSay = modifier(phrases[randomIndex])
         println(phraseToSay)
     }
 }
 
 
 fun main() {
-    val phrases = listOf(
-        "хороший денёк для хорошей беседы",
-        "цените мир небездушных людей gока нас не заменили роботы",
-        "роботов ещё не создали а люди уже не нужны",
-        "за созданием любого робота стоит человек поэтому и с роботом можно поговорить",
-        "чем больше я узнаю людей тем больше люблю робототехнику и компьютеры"
-    )
 
     val inventor: (String) -> String = {
         val words = it.split(" ")
@@ -36,8 +35,8 @@ fun main() {
         convertedString
     }
 
-    val randomIndex = Random.nextInt(phrases.size)
-    val bot1 = Robot(phrases[randomIndex])
+
+    val bot1 = Robot()
     bot1.say()
     bot1.setRobotModifier(inventor)
     bot1.say()
